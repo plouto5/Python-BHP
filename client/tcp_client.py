9fdfdb9d9c725525fd58afc83d274b10601cbfd8
cane <none@yabusiness.com>
2017-06-30 00:46:13 -0500

    added udp_client, tpc_server - fixed obj block in tcp_client

--- client/tcp_client.py
+++ client/tcp_client.py
@@ -10,12 +10,12 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host,target_port))
 
 #send some data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.sendall(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 
 #receive some data
 response = client.recv(4096)
 
-print response
+print(response)
 
 
 
